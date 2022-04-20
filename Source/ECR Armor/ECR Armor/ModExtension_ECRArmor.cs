073cdf6071f2b9c3225632c14100386b8612ff96
whitegrimreaper <theDumb12@yahoo.com>
2022-04-20 10:24:59 -0700

    Initial commit with base armor and helmet

---
+++ Source/ECR Armor/ECR Armor/ModExtension_ECRArmor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RimWorld;
+using Verse;
+
+namespace ECR_Armor
+{
+    public class ModExtension_ECRArmor : DefModExtension
+    {
+        public float addHediffChance = 0.05f;
+        public HediffDef hediffToAdd;
+    }
+}
